6c7dcd733aed2731760b8632214efceac9b66728
JunyanSu <sujy@shanghaitech.edu.cn>
2018-11-15 15:46:49 -0800

    Finish the GUI prototype; change image names

--- GUI/LegoBuilder.py
+++ GUI/LegoBuilder.py
@@ -4,8 +4,9 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
 
-
+# the front page widget that is in charge of front page
 class FrontPageWidget(QWidget):
 
     def __init__(self):
@@ -74,19 +75,7 @@ class FrontPageWidget(QWidget):
         choose_mode_label.move(403,339)
 
 
-class PicButton(QAbstractButton):
-    def __init__(self, pixmap, parent=None):
-        super(PicButton, self).__init__(parent)
-        self.pixmap = pixmap
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.drawPixmap(event.rect(), self.pixmap)
-
-    def sizeHint(self):
-        return self.pixmap.size()
-
-## class for two modes
+## parent class for the two modes
 class ModeWidget(QWidget):
     def __init__(self):
         super().__init__()
@@ -124,24 +113,158 @@ class ModeWidget(QWidget):
         btn.move(23,45.3)
         btn.clicked.connect(self.goFrontPage)
 
+    def setButton(self,img_path,size,pos,func):
+
+        img = QPixmap(img_path)
+        img = img.scaled(*size)
+        btn = PicButton(img, self)
+        btn.move(*pos)
+        if (func):
+            btn.clicked.connect(func)
+
+    def setLebal(self,img_path,size,pos):
+        img = QPixmap(img_path)
+        img = img.scaled(*size)
+        label = QLabel(self)
+        label.setPixmap(img)
+        label.move(*pos)
+
 
 class BuildModeWidget(ModeWidget):
     def __init__(self,parent=None):
         super().__init__()
+        self.setLayout()
+        self.grid = GridWidget(self)
+
+    def setLayout(self):
         self.resize(1245,787)
         self.setBackgroudColor()
         self.center()
         self.setGoFrontPageButton("GUI-fig/BuildMode/BuildMode.png")
+        self.setLebal("GUI-fig/BuildMode/ChooseBlock.png",(220,50),(50,190))
+        self.setLebal("GUI-fig/BuildMode/SearchForPrebuild.png",(220,50),(916,190))
+        self.setButton("GUI-fig/BuildMode/ComingSoon.png",(242,92),(53,258),None)
+        self.setButton("GUI-fig/BuildMode/ComingSoon.png",(242,92),(53,388),None)
+        self.setButton("GUI-fig/BuildMode/ComingSoon.png",(242,92),(53,518),None)
+        self.setButton("GUI-fig/BuildMode/ComingSoon.png",(242,92),(53,648),None)
 
+
+class PicButton(QAbstractButton):
+    def __init__(self, pixmap, parent=None):
+        super(PicButton, self).__init__(parent)
+        self.pixmap = pixmap
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.drawPixmap(event.rect(), self.pixmap)
+
+    def sizeHint(self):
+        return self.pixmap.size()
+
+
+class LegoWidget(QWidget):
+    def __init__(self,parent=None,position = (0,0) , size = (30,30),color = '#ff0000'):
+        super(LegoWidget,self).__init__(parent)
+        self.size = size
+        self.color = color
+        self.setGeometry(*position,*size )
+        self.show()
+
+    def paintEvent(self, e):
+
+        qp = QPainter()
+        qp.begin(self)
+        self.drawShape(qp)
+        qp.end()
+
+    def drawShape(self,qp):
+        col = QColor(0, 0, 0)
+        col.setNamedColor(self.color)
+        qp.setPen(col)
+
+        qp.setBrush(QColor(self.color))
+        qp.drawRect(0, 0, * self.size)
+
+    def setSetting(self,size,color):
+        self.size = size
+        self.color = color
+        print("test")
+
+class GridWidget(QWidget):
+    def __init__(self, parent=None):
+        super(GridWidget, self).__init__(parent)
+        self.initUI()
+        self.setMouseTracking(True)
+
+    def initUI(self):
+        # the mouse x and mouse y
+        self.mouse_x = 0
+        self.mouse_y = 0
+        self.x = 367
+        self.y = 234
+        self.w = 480
+        self.h = 480
+        self.resolution = 32
+        self.step = int(self.w / self.resolution)
+        self.setGeometry(self.x, self.y, self.w , self.h )
+        self.cur_lego_shape = LegoWidget(self)
+        #self.setGeometry(0, 0, 1245,787)
+
+        #self.label = QLabel(self)
+        #self.label.resize(200, 40)
+        self.show()
+
+    def mouseMoveEvent(self, event):
+        self.mouse_x = event.x()
+        self.mouse_y = event.y()
+        self.cur_lego_shape.move(*self.mouseToGrid())
+        #print((event.x(),event.y()))
+
+    def mousePressEvent(self,event):
+        self.cur_lego_shape = LegoWidget(self,position =self.mouseToGrid())
+        #self.cur_lego_shape.setSetting((60,60),'#00ff00')
+
+
+    def mouseToGrid(self):
+        # change from mouse coordinate to the grid coordinate
+        (x,y) = (self.mouse_x,self.mouse_y)
+        x = int(x/self.step) * self.step
+        y = int(y/self.step) * self.step
+        return (x,y)
+
+    def paintEvent(self, e):
+
+        qp = QPainter()
+        qp.begin(self)
+        self.drawGrid(qp)
+        qp.end()
+
+    def drawGrid(self,qp):
+        pen = QPen(Qt.black, 2, Qt.SolidLine)
+        qp.setPen(pen)
+        # draw columns
+        for x in range(0,self.h + self.step,self.step):
+            qp.drawLine(x,0,x,self.w)
+            #print(x)
+        for y in range(0,self.w + self.step,self.step):
+            qp.drawLine(0,y,self.h,y)
+            #print(x)
 
 class GameModeWidget(ModeWidget):
     def __init__(self,parent=None):
         super().__init__()
+        self.setLayout()
+
+    def setLayout(self):
         self.resize(1245,787)
         self.setBackgroudColor()
         self.center()
         self.setGoFrontPageButton("GUI-fig/GamesModePage/GameMode.png")
-
+        ## set the buttons
+        self.setButton("GUI-fig/GamesModePage/ticTacToe.png",(438,156),(164,301),None)
+        self.setButton("GUI-fig/GamesModePage/Checkers.png",(438,156),(643,301),None)
+        self.setButton("GUI-fig/GamesModePage/comingSoon.png",(438,156),(164,486),None)
+        self.setButton("GUI-fig/GamesModePage/comingSoon.png",(438,156),(643,486),None)
 
 
 
